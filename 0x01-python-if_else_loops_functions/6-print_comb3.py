8db602bfe657b932a7ec7241cd2c3e9f6dca9331
mikek1337 <mikekegnlyew@gmail.com>
2022-09-20 18:10:49 +0300

    fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for i in range(0, 10):
     j = i + 1
     while j <= 9:
-        print("{x}{j}".format(x=i, j=j), end='\n' if i == 8  else ', ')
+        print("{x}{j}".format(x=i, j=j), end='\n' if i == 8 else ', ')
         j += 1
