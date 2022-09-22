6aa0f000feda398c9ccf5898c2f3829592a367d9
mikek1337 <mikekegnlyew@gmail.com>
2022-09-22 22:39:58 +0300

    fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     if arglen == 0:
         print("{len} arguments.".format(len=arglen))
     elif arglen == 1:
-        print("{len} argument.".format(len=arglen))
+        print("{len} argument:".format(len=arglen))
         print("{len}: {arg}".format(len=arglen, arg=argv[1]))
     else:
         print("{len} arguments:".format(len=arglen))
