bb7e41258dfeb586ce8d7e690683566a231a3dc8
mikek1337 <mikekegnlyew@gmail.com>
2022-09-25 17:04:20 +0300

    remove c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    my_string = [*my_string]
+    new_string = ''
+    for i in my_string:
+        if i == 'c' or i == 'C':
+            continue
+        else:
+            new_string += i
+    return new_string
