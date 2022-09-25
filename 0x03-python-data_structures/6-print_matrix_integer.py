73281394f511df3fb4ff559e2495c81b2cc5d023
mikek1337 <mikekegnlyew@gmail.com>
2022-09-25 19:06:49 +0300

    matrix list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
             print("{:d}".format(j), end='')
+            if 3 != (len(i) - 1):
+                print(" ", end="")
         print("")
