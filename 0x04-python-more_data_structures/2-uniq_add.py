3c77aa5a0dfff237355411d6473fab8cd1acd137
mikek1337 <mikekegnlyew@gmail.com>
2022-09-28 20:59:00 +0300

    set

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    new_list = []
-    i = 0
     sum = 0
-    while i < len(my_list):
-        count = i + 1
-        while count < len(my_list) - 1:
-            if my_list[count] == my_list[i]:
-                my_list[count] = 0
-            count += 1
-        i += 1
-
-    for i in my_list:
+    for i in set(my_list):
         sum += i
 
     return sum
