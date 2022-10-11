45c4d0584c72309d0a82aba398511b5df9be18e7
mikek1337 <mikekegnlyew@gmail.com>
2022-10-11 13:06:38 +0300

    tet

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,9 +6,9 @@ class Square:
     '''Represent square'''
 
     def __init__(self, size=0):
-        if (type(size) is not int):
+        if not isinstance(size, int):
             raise TypeError("size must be integer")
-        if (size < 0):
+        elif size < 0:
             raise ValueError("size must be >=0")
         self.__size = size
 
