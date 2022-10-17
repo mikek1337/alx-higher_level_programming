78dfe451401f5e98c5296dfe3c333a2ba1800729
Mikek1337 <mikekegnlyew@gmail.com>
2022-10-17 15:23:35 +0300

    fixed coding style

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -23,7 +23,6 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-
         self.__height = value
 
     @property
@@ -36,7 +35,6 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-
         self.__width = value
 
     def area(self):
