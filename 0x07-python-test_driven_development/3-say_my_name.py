ef64cd03cae78208f488a23588dccce0c819b0a3
Mikek1337 <mikekegnlyew@gmail.com>
2022-10-18 11:21:17 +0300

    git add

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,13 +2,13 @@
 
 "Module say_my_name"
 
+
 def say_my_name(first_name, last_name=""):
     """
     Prints the first name and last name 
     """
-    
     if not isinstance(first_name, str):
-        TypeError("first_name must be a string")
+       raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
-        TypeError("last_name must be a string")
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
