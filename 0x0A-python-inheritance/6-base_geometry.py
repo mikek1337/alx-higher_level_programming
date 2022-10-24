092a72be9c838f73c4d662813355f48927e78139
mikek1337 <mikekegnlyew@gmail.com>
2022-10-24 21:26:26 +0300

    imporve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Module for base geometry class hope this explains needs more"""
+
+
+class BaseGeometry:
+    """Base class for geometry hope this explains needs more """
+    def area(self):
+        """unimplemented area method this doc exists"""
+        raise Exception("area() is not implemented")
+        
