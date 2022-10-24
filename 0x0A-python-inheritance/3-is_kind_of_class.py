141032a4da6636c1a7114d6b1f5e8cd0320f0f33
Mikek1337 <mikekegnlyew@gmail.com>
2022-10-24 15:09:33 +0300

    same instance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Module to determine the instance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if it is the same instance"""
+    return isinstance(obj, a_class)
