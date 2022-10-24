d1340bc1101713e0bb083eb418abbf2e80e9104f
Mikek1337 <mikekegnlyew@gmail.com>
2022-10-24 15:04:13 +0300

    is_same_class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""module to verify object"""
+
+
+def is_same_class(obj, a_class):
+    """returns true of the object type match else false"""
+    return type(obj) == a_class
