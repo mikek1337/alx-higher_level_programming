4df5f4707c9354ed005e3695bbde3777f35ca262
Mikek1337 <mikekegnlyew@gmail.com>
2022-10-24 14:01:01 +0300

    added doc

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-"""
-object lookup
-"""
+"""Module for listing objects"""
+
 
 def lookup(obj):
-    """ function that return objects methods"""
+    """function that return objects methods"""
     return dir(obj)
