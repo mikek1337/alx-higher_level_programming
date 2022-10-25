86cebae4bf1493aaffd51f9cbeba13f9ddc40ef4
Mikek1337 <mikekegnlyew@gmail.com>
2022-10-25 14:57:17 +0300

    fix doc

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-"""Module to start in json"""
+"""Module to start in json printing and converting json"""
 import json
+
 
 def to_json_string(my_obj):
     """function to convert json object to string"""
