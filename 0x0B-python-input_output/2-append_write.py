e16982eb479addc115336cd3fc6db6d7e064dc30
Mikek1337 <mikekegnlyew@gmail.com>
2022-10-25 14:31:30 +0300

    append text to file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Module to append data to file without destroying data"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
