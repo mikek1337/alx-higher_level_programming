862a1ef44d090b293ed45152940bd70986fa95e3
Mikek1337 <mikekegnlyew@gmail.com>
2022-10-25 12:27:50 +0300

    added comments

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-"""Module for file input output function"""
+"""Module for file input output function how long does this comment has to be"""
 
 
 def read_file(filename=""):
-    """Opens file in read mode and do nothing"""
+    """Opens file in read mode and do nothing i guess really long hope this passes"""
     with open(filename, 'r',encoding="utf-8") as f:
         print(f.read(),end="")
