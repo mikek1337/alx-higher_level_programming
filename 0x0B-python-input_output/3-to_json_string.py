a443496d6b5a3a98da17a8c28a82b020e0f7dd9f
Mikek1337 <mikekegnlyew@gmail.com>
2022-10-25 14:55:25 +0300

    fixed dump

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 """Module to start in json"""
-
+import json
 
 def to_json_string(my_obj):
     """function to convert json object to string"""
-    return str(my_obj)
+    return json.dumps(my_obj)
