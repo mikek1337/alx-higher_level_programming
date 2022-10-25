3373e8d1b7d0bff486125c65c56390f54fc91aef
Mikek1337 <mikekegnlyew@gmail.com>
2022-10-25 14:17:22 +0300

    fixed doc

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,6 @@
 """Module to write string to file"""
 
 def write_file(filename="", text=""):
+    """function to write to file"""
     with open(filename, "w", encoding="utf-8") as f:
-       return  f.write(text)
+       return f.write(text)
