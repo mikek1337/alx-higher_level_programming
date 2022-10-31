e256e4b806c86f1251ed7f858a0ee84d3dad739f
Mikek1337 <mikekegnlyew@gmail.com>
2022-10-31 14:42:03 +0300

    int atom

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+
+"""unit test module"""
+
+import unittest
+from models.base import Base
+class TestStringMethods(unittest.TestCase):
+    """Test class"""
+    def test_case(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
+
+        b5 = Base()
+        self.assertEqual(b5.id, 4)
+
+if __name__ == '__main__':
+    unittest.main()
