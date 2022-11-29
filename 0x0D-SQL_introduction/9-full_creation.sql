6d9875cba200e22696cc0fd4c4212773f2c0f027
mikek1337 <mikekegnlyew@gmail.com>
2022-11-29 09:48:02 +0300

    create and insert

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- create and save data to table
+CREATE TABLE IF NOT EXISTS second_table(
+id INT,
+name VARCHAR(256),
+score INT
+);
+-- insert to the table second_table
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
+
