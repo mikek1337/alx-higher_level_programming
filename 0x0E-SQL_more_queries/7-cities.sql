1b9582a414b2d80cd74cdbd58fd97e695e548c92
mikek1337 <mikekegnlyew@gmail.com>
2022-11-30 20:34:32 +0300

    foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- foregin key constraint
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    state_id INT FOREIGN KEY REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
+);
