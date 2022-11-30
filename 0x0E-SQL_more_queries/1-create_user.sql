a2d5153f716d1c229ef530dfc3515dfa18edf457
mikek1337 <mikekegnlyew@gmail.com>
2022-11-30 19:20:14 +0300

    pri

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Create and grant permission for user
-CREATE USER IF NOT EXISTS user_0d_1;
+CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1;
