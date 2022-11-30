792f94d173b645549988eb295c07e8191429fea6
mikek1337 <mikekegnlyew@gmail.com>
2022-11-30 22:02:00 +0300

    fix

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Join cities and states
-SELECT c.id , c.name , s.name FROM states as s
-INNER JOIN cities as c ON s.id = c.state_id ORDER BY c.id;
+SELECT c.id , c.name , s.name FROM cities as c
+INNER JOIN states as s ON c.state_id = s.id ORDER BY c.id;
 
