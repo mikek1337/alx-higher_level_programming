58042161acddaf19c742f8e5d98e47e66999565e
Mikek1337 <mikekegnlyew@gmail.com>
2022-11-30 12:02:23 +0300

    fix

--- 0X0E-SQL_more_queries/0-privileges.sql
+++ 0X0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Show user privlage
-SHOW GRANTS FOR `user_0d_1`@`localhost`;
-SHOW GRANTS FOR `user_0d_2`@`localhost`;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
