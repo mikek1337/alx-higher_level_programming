0f77a63be7f585d7c07d0cf6b455d99cea96576b
mikek1337 <mikekegnlyew@gmail.com>
2022-12-19 20:06:09 +0300

    arg

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-import { argv } from 'node:process';
-
-if (argv.length > 0) {
+if (process.argv.length > 2) {
   console.log('Argument found');
 } else { console.log('No argument'); }
