d5e07251fc82c5056ccfa3d06aa03085e7c87b01
mikek1337 <mikekegnlyew@gmail.com>
2022-12-19 22:15:22 +0300

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const n = Math.floor(process.argv[2]);
+console.log(factorial(n));
+function factorial(n){
+    if (n == 1){
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
