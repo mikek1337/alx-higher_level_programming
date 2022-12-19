7a0ceef8d4d06311358f729bff985229ef791efe
mikek1337 <mikekegnlyew@gmail.com>
2022-12-19 20:39:13 +0300

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-if (process.argv.length == 2) {
-  console.log('Argument found');
-} else { console.log('No argument'); }
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Arguments found');
+} else { console.log('Arguments found'); }
