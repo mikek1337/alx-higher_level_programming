bfb142de0deb9c1f30838ad9d0ac51b1d5bc0a09
mikek1337 <mikekegnlyew@gmail.com>
2022-12-19 23:00:55 +0300

    maybe

--- 0x12-javascript-warm_up/main.js
+++ 0x12-javascript-warm_up/main.js
@@ -1,5 +1,5 @@
-#!/usr/bin/node
-const callMeMoby = require('./101-call_me_moby').callMeMoby;
-callMeMoby(3, function () {
-  console.log('C is fun');
+#!/snap/bin/node
+const addMeMaybe = require('./102-add_me_maybe').addMeMaybe;
+addMeMaybe(4, function (nb) {
+  console.log('New value: ' + nb);
 });
