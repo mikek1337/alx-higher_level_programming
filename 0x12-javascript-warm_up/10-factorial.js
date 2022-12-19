3864ed9961ed6b5967815af80a2968e0a4f13a53
mikek1337 <mikekegnlyew@gmail.com>
2022-12-19 22:19:49 +0300

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,12 @@
-#!/usr/bin/node
+#!/snap/bin/node
 const n = Math.floor(process.argv[2]);
 console.log(factorial(n));
 function factorial (n) {
   if (n === 1) {
     return 1;
   }
+  if (n === 0){
+      return 0;
+  }  
   return n * factorial(n - 1);
 }
