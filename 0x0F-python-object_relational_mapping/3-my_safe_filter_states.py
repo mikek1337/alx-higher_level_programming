fe79206d9e62ffec51d366d04c7c6667bc072322
mikek1337 <mikekegnlyew@gmail.com>
2022-12-21 22:45:52 +0300

    sql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,11 +7,11 @@ import MySQLdb
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(
-        host="172.17.0.1", user=sys.argv[1], password=sys.argv[2], database=sys.argv[3], port=1234)
+        host="localhost", user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
     data = conn.cursor()
     query = "SELECT * FROM states WHERE name={statename} ORDER BY id".format(
         statename='"'+sys.argv[4]+'"')
-    data.execute(query)
+    data.execute("""SELECT * FROM states WHERE name = %s""", (sys.argv[4],))
     result = data.fetchall()
 
     for state in result:
