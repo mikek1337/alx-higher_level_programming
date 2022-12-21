230df14661f14585a7484eac566821f161995fbe
mikek1337 <mikekegnlyew@gmail.com>
2022-12-21 21:40:49 +0300

    filter by name

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Search by state name."""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost", user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
+    data = conn.cursor()
+    query = "SELECT * FROM states WHERE name={statename} ORDER BY id".format(
+        statename='"'+sys.argv[4]+'"')
+    data.execute(query)
+    result = data.fetchall()
+
+    for state in result:
+        print(state, end="\n")
