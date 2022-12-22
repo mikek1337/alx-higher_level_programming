f71473923fef179fec22fad5444af725bb3c31c1
Mikek1337 <mikekegnlyew@gmail.com>
2022-12-22 15:48:06 +0300

    update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,11 +3,11 @@
 import sys
 import MySQLdb
 if __name__ == "__main__":
-
-    conn = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
-    data = conn.cursor()
-    data.execute(
-        "SELECT cities.id ,cities.name, states.name FROM cities INNER JOIN states on states.id = cities.state_id ORDER BY cities.id")
-    result = data.fetchall()
-    for city in result:
-        print(city)
+    if len(sys.argv) > 1:
+        conn = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+        data = conn.cursor()
+        data.execute(
+            "SELECT cities.id ,cities.name, states.name FROM cities INNER JOIN states on states.id = cities.state_id ORDER BY cities.id")
+        result = data.fetchall()
+        for city in result:
+            print(city)
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""List all cities with a given name"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        conn = MySQLdb.connect(
+            "localhost", sys.argv[1], "", sys.argv[2])
+        data = conn.cursor()
+        data.execute(
+            """SELECT cities.id ,cities.name, states.name FROM cities INNER JOIN states on states.id = cities.state_id WHERE states.name=%s ORDER BY cities.id""", (sys.argv[3],))
+        result = data.fetchall()
+        i = 0
+        for state in result:
+            print(state[1],end="")
+            i+=1
+            if len(result) > i:
+                print(", ", end="") 
