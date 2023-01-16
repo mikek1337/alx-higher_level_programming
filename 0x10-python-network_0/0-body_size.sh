575bb57f569389ada5b1e1cf79902a8e36454e48
mikek1337 <mikekegnlyew@gmail.com>
2023-01-16 20:22:06 +0300

    please work

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays size of a site using curl
-curl -w '%{size_download}\n' -sI --output /dev/null --no-progress-meter -N "$1"
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
