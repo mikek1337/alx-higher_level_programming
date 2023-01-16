5f99d6a04be3ea1f4458bb9e33a763f8b3a70a4b
mikek1337 <mikekegnlyew@gmail.com>
2023-01-16 22:24:13 +0300

    FINAL ANSWER

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send post request
-curl -sd "email=test@gmail.com&subject=i will always be here for PLD" "$1" -X post
+curl -sd "email=test@gmail.com&subject=i will always be here for PLD" "$1" -X POST
