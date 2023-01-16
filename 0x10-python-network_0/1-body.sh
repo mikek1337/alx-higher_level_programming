fe48902e1509d57a27f4d13992a16263d3ee68f1
mikek1337 <mikekegnlyew@gmail.com>
2023-01-16 20:42:24 +0300

    q1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # display response body if status 200
-curl -w ' %{http_code}'  0.0.0.0:5000/route_1 -s -L | grep 200 | sed 's/200/ /g'
-  
+curl -w ' %{http_code}' "$1" -s -L | grep 200 | sed 's/200/ /g'
