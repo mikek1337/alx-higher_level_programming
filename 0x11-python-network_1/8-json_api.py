025da0a9a2fd63ac96635931b478bcc92cb51a33
mikek1337 <mikekegnlyew@gmail.com>
2023-01-17 22:56:34 +0300

    q8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Search by alphabets."""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
