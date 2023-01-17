3658b87fbbb05391ad2dc040fecd5df2e600482c
Mikek1337 <mikekegnlyew@gmail.com>
2023-01-17 16:13:28 +0300

    q0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,8 @@
+import urllib.request
+
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    the_page = response.read()
+    print("Body response:")
+    print("\t - type: {type} \n\t - content: {res} \n\t - utf8 content: {content}".format(
+        type=type(response.read()), res=the_page, content=the_page.decode()))
