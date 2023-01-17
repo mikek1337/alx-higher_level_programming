e57ae4f6b16fc8991ed043bf9aa833659c54c3ea
mikek1337 <mikekegnlyew@gmail.com>
2023-01-17 22:49:06 +0300

    fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,8 +5,9 @@
 if __name__ == "__main__":
     import requests
     import sys
-    try:
-        res = requests.get(sys.argv[1])
+
+    res = requests.get(sys.argv[1])
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
         print(res.text)
-    except requests.HTTPError as e:
-        print("Error code: {}".format(e.errno))
