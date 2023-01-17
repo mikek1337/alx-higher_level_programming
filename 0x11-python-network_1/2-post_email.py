9001e5837209b2eb478f8194253e925cbe867396
mikek1337 <mikekegnlyew@gmail.com>
2023-01-17 21:04:38 +0300

    q3

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,4 +9,3 @@ if __name__ == "__main__":
         sys.argv[1], data={"email": sys.argv[2]}, method="POST")
     with urllib.request.urlopen(req) as response:
         content = response.read()
-        print(content)
