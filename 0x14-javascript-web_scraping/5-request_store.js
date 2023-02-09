c7eb3690dadc370b1b9aaf45188e3b33338972bc
mikek1337 <mikekegnlyew@gmail.com>
2023-02-09 21:17:10 +0300

    initial fix

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const fileUrl = process.argv[3];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(fileUrl, body, 'utf-8');
+  }
+});
