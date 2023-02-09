270d6b91630ad7178f25bea492d4e071c8ed6181
mikek1337 <mikekegnlyew@gmail.com>
2023-02-09 20:35:11 +0300

    initial fix

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const process = require('process');
+const request = require('request');
+
+const args = process.argv;
+
+if (args.length > 2) {
+  request.get(args[2], function (error, response, request) {
+    if (!error) { console.log('code: ' + response && response.statusCode); } else { console.log(error); }
+  });
+}
