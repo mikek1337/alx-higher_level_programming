a0a2c071739ec3f61dd3f451ec496ea45455ac51
mikek1337 <mikekegnlyew@gmail.com>
2023-02-09 20:58:41 +0300

    initial fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -7,6 +7,6 @@ const args = process.argv;
 
 if (args.length > 2) {
   request.get(args[2], function (error, response, request) {
-    if (!error) { console.log('code: ' + response && response.statusCode); } else { console.log(error); }
+    if (!error) { console.log('code: ' + response.statusCode); } else { console.log(error); }
   });
 }
